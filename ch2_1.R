f401d55b7244724fec510b41d5ab78611b395380
AdamWynne <awynne@gmail.com>
2016-01-18 20:59:20 -0500

    first commit

---
+++ ch2_1.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+players <- c("Ricky Henderson", "Lou Brock", "Ty Cobb",
+             "Eddie Collins", "Max Carey", "Joe Morgan",
+             "Luis Aparicio", "Paul Molitor", "Roberto Alomar")
+
+SB <- c(1406,938,897,741,738,689,506,504,474)
+CS <- c(335,307,212,195,109,162,136,131,114)
+G <- c(3081,2616,3034,2826,2476,2649,2599,2683,2379)
+
+SB.Attempt = SB + CS
+
+Success.Rate = SB/SB.Attempt
+
+SB.Game = SB / G
+
+plot(SB.Game,Success.Rate)
+
